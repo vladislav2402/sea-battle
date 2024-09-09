112c3450a7bd2f916e8ef6ff2f080c66f0f4cdd1
Siberian <72248804+vladislav2402@users.noreply.github.com>
2024-09-09 13:46:22 +0200

    static init

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let myVid = document.getElementById("myPlayer");
+myVid.volume = 0.2;
+
+document.getElementById('info').onclick = () => {
+	alert('Курсова робота\n Морський бій\n Студентки ІПЗ-31\n Бурлуцької Юлії')
+}
